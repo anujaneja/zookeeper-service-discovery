02a46539775d00e9f32e97b8b1038e2439fce361
Anuj Aneja <anuj.aneja@unicommerce.com>
2018-04-23 21:05:42 +0530

    Service discovery with multiple servers which act as load balancer is implemented.

--- service-provider/src/main/java/com/anujaneja/zookeeper/serviceprovider/controllers/ServiceProviderController.java
+++ service-provider/src/main/java/com/anujaneja/zookeeper/serviceprovider/controllers/ServiceProviderController.java
@@ -1,13 +1,24 @@
 package com.anujaneja.zookeeper.serviceprovider.controllers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class ServiceProviderController {
 
     @GetMapping("/helloWorld")
     public String helloWorld1(){
-        return "Hello World";
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String url = request.getRequestURL().toString() + "?" + request.getQueryString();
+
+        return "Welcome to service1 : "+ url;
     }
 }
